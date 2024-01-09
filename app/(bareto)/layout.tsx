01bef92b940a154f4364c807a01d2fb52efe4be4
noisk8 <juansk8jaramillo@gmail.com>
2024-01-08 22:55:13 -0500

    navbar  🚬

--- app/(bareto)/layout.tsx
+++ app/(bareto)/layout.tsx
@@ -1,15 +1,19 @@
+import { Navbar } from "@/components";
 import React from "react";
 
-export default function AboutLayout({
+export default function BaretoLayout({
  children
 }: {
  children: React.ReactNode;
 }) {
   return (
+    <>
+    <Navbar />
    <main className="flex flex-col items-center p-24">
 
 <span className="text-gl"> Nea paguelo pues</span>
 {children}
    </main>
+   </>
   );
 }
