67b2162c3bce0a411beaca5d7d4ec90c972d5438
noisk8 <juansk8jaramillo@gmail.com>
2024-01-09 22:10:00 -0500

    docker nuevo

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Image from 'next/image'
+import Link from 'next/link'
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <span className="text-center text-5xl font-bold text-cyan-300 animate-pulse">  Hola Gonorrea</span>
+    <Link href="/about" >
+
+      <button> about</button>
+    </Link>
     </main>
   )
 }
