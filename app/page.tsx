28387fe385ea0e570ff4453b90357aaca89c9b79
Noisk8 <juansk8jaramillo@gmail.com>
2025-01-20 01:59:42 -0500

    ninja cambio

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,8 @@ export default function Home() {
       <span className="text-center text-5xl font-bold text-cyan-300 animate-pulse">  Hola Gonorrea</span>
     <Link href="/about" >
 
-      <button> about</button>
+      <button> neasss
+      </button>
     </Link>
     </main>
   )
